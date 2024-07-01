c657400c29e47d591d18d5011279583584bad321
fair-dao-com <fairdao@hotmail.com>
2024-07-01 16:13:34 +0800

    create project

---
+++ src/wallet/fair.extensions.wallet/entity/AccountData.cs
@@ -0,0 +1,19 @@
+﻿using fair.extensions.wallet.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fair.extensions.wallet.entity
+{
+
+    /// <summary>
+    /// 账号数据
+    /// </summary>
+    public class AccountData
+    {
+
+        public List<TronCoin>? TronAccounts { get; set; } = new ();
+    }
+}
